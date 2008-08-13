37d6a318e711be25e9ada4a0b80163a3d38c5b95
hradec <hradec@c7932f9d-8d50-0410-b51a-5f8c8da6f427>
2008-08-13 06:53:08 +0000

    tagged

---
+++ tags/alphaB/python/slum/parameter.py
@@ -0,0 +1,51 @@
+#
+# parameter - classes to define slum template parameters and UI formating
+#
+#    Copyright (C) 2008 - Roberto Hradec
+#
+# ---------------------------------------------------------------------------
+#	 This file is part of SLUM.
+#
+#    SLUM is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    SLUM is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with SLUM.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------------
+
+class parameter:
+	def __init__(self, value, name, help='No Help available', max=1, min=0, output=False, ui=None, callback=None):
+		self.name	 	= name
+		self.help	 	= help
+		self.max	 	= max
+		self.min	 	= min
+		self.ui 		= ui
+		self.output		= output
+		self.value  	= value
+		self.callback 	= callback
+
+class group:
+	def __init__(self, value, name, help='No Help available', opened=True):
+		self.name 	= name
+		self.help 	= help
+		self.opened = opened
+		self.value  = value
+
+class ui:
+	class  popup:
+		def __init__(self, values = {'yes':True, 'no':False} ):
+			self.values = values
+	class checkbox:
+		def __init__(self):
+			pass
+	class button:
+		def __init__(self,callback, name=''):
+			self.name		= name
+			self.callback	= callback
---
+++ tags/alphaB/python/slumMaya/slumNode.py
@@ -0,0 +1,42 @@
+#
+# slumNode -  classNode that evaluates the slum class stored in a node and
+#			  makes its object available as self.slum
+#
+#    Copyright (C) 2008 - Roberto Hradec
+#
+# ---------------------------------------------------------------------------
+#	 This file is part of SLUM.
+#
+#    SLUM is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    SLUM is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with SLUM.  If not, see <http://www.gnu.org/licenses/>.
+# ------------------------------------------------------------
+
+
+from classNode import *
+import slum
+
+class slumNode(classNode):
+	'''
+		derivated from classNode, this class adds the advantage of evaluating an slum attribute automaticaly, if
+		one is present in a node.
+		Instead of returning the string value of the slum attribute, it returns the evaluated class object
+		from the code in the string attribute.
+	'''
+	def __init__(self, node, data=None):
+		classNode.__init__(self, node, data)
+		self.evalSlumClass()
+	def evalSlumClass(self):
+		if classNode.has_key(self, 'slum'):
+			classe = classNode.__getitem__(self,'slum')
+			if classe:
+				self.slum = slum.evalSlumClass(classe['code'], classe['name'])
