c2b5505a2a5ea6ecce69076f436543f1cd6cb389
hradec <hradec@c7932f9d-8d50-0410-b51a-5f8c8da6f427>
2008-08-21 02:34:48 +0000

    datatypes now set values direct to __dict__ instead of using exec and eval. much faster and cleaner. also, we can now derivate color to create datatypes with more than 3 elements, like r,g,b,a or x,y,z,w
    adding compile shader and swatch render methods in delight.py. 
    Added setInternalValueInContext method to renderer classes so
    we can do something when a value is set in a node attribute (like re-render the swatch automatically)
    
    now shaderbase only calls renderer methods if they exist.

--- python/slum/datatypes.py
+++ python/slum/datatypes.py
@@ -24,59 +24,74 @@ import math
 
 class color:
 	''' slum datatype to hold color data.
-	The "internal" variable is a list that defines all the possible keys for this datatype. As default,
+	The "internal" variable is a list that defines all the possible keys for this datatype. For color,
 	they are [r,g,b], but they can be overriden by derivated classes, like vector, which defines it as
 	x,y,z.
 	This datatype is defined to simplify a shader implementation, specially when defining slum
 	shader parameters.'''
 
 	internal = ['r','g','b']
-	def __init__(self, data=0, data1=None, data2=None):
+	def __init__(self, data=0, *args):
 		self.checkValue(data)
-		d = str(data)
-		exec( 'self.%s = %s' % (self.internal[0], d) )
-		exec( 'self.%s = %s' % (self.internal[1], d) )
-		exec( 'self.%s = %s' % (self.internal[2], d) )
-		if data1!=None:
-			self.checkValue(data1)
-			self.checkValue(data2)
-			exec( 'self.%s = %s' % (self.internal[1], str(data1)) )
-			exec( 'self.%s = %s' % (self.internal[2], str(data2)) )
+		for each in self.internal:
+			self.__dict__[each] = data
+		nargs = len(args)
+		if nargs:
+			if nargs==len(self.internal)-1:
+				internalIndex=1
+				for each in args:
+					self.checkValue(each)
+					self.__dict__[self.internal[internalIndex]] = each
+					internalIndex += 1
+			else:
+				raise Exception('dataype requires 1 or %d parameters in initialization. Received only %d parameters.' % (len(self.internal),nargs+1))
 	def checkKey(self, key):
-		if key>2 or key<0:
+		if key>len(self.internal) or key<0:
 			raise Exception('Color values can only have 3 elements. Element %d not valid' % key )
 	def checkValue(self, value):
 		if type(value) not in [int,long,float]:
 			raise Exception('Data type "%s" not supported' % type(value))
-	def createData(self):
-		self.data = [
-			eval('self.%s'% self.internal[0]),
-			eval('self.%s'% self.internal[1]),
-			eval('self.%s'% self.internal[2]),
-		]
 	def __getitem__(self, key):
 		self.checkKey(key)
-		return eval('self.%s'% self.internal[key])
+		return self.__dict__[self.internal[key]]
 	def __setitem__(self, key, value):
 		self.checkKey(key)
 		self.checkValue(value)
-		exec('self.%s = %s'% (self.internal[key],str(value)) )
+		self.__dict__[self.internal[key]] = value
 	def __repr__(self):
-		self.createData()
-		return '%s(%s,%s,%s)' % (self.__class__.__name__, str(self.data[0]), str(self.data[1]), str(self.data[2]))
+		data = []
+		for each in self.internal:
+			data.append("%s" % str(self.__dict__[each]))
+		return '%s(%s)' % (self.__class__.__name__,','.join(data))
 	def __len__(self):
 		return 3
 	def __delitem__(self, key):
 		pass
 
 class vector(color):
+	'''
+		slum datatype to hold vector data.
+		its derivated from color, overriding the internal to x,y,z.
+		also, theres 2 added method to manipulate vectors: lenght and normalize
+	'''
 	internal = ['x','y','z']
 	def length(self):
-		self.createData()
-		return math.sqrt( (self.data[0] * self.data[0]) + (self.data[1] * self.data[1]) + (self.data[2] * self.data[2]) )
+		return math.sqrt(
+			(self.__dict__[self.internal[0]] * self.__dict__[self.internal[0]]) +
+			(self.__dict__[self.internal[1]] * self.__dict__[self.internal[1]]) +
+			(self.__dict__[self.internal[2]] * self.__dict__[self.internal[2]])
+		)
 	def normalize(self):
 		len = self.length()
-		return vector(self.data[0]/len, self.data[1]/len, self.data[2]/len)
+		return vector(
+			self.__dict__[self.internal[0]]/len,
+			self.__dict__[self.internal[1]]/len,
+			self.__dict__[self.internal[2]]/len
+		)
 
 class normal(vector):
+	'''
+		slum datatype to hold normal data.
+		its exactly like vector. Its defined just as a placeholder for people used to rsl. (like me :D)
+	'''
 	pass
